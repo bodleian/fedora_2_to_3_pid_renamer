72a50a0bdc8a0dd5138f904369b74368e64df585
Rob Nichols <git@nicholshayes.co.uk>
2015-09-25 14:29:01 +0100

    Bump to version 0.0.2

--- lib/fedora_2_to_3_pid_renamer/version.rb
+++ lib/fedora_2_to_3_pid_renamer/version.rb
@@ -1,9 +1,11 @@
 module Fedora2To3PidRenamer
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
 
 # History
 # =======
+# 
+# 0.0.2 - Adds modification of cmodel-n-deployments.txt 
 #  
 # 0.0.1 - Initial working app
 #
